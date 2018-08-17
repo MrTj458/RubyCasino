71ddfc0a6ed1f57d6ba3ae94a78d78f96fe817b9
Trevor H <mrtj458@gmail.com>
2018-08-17 15:18:57 -0600

    Remove tests and added comments on DoubleYourMoney.rb

--- DoubleYourMoney.rb
+++ DoubleYourMoney.rb
@@ -4,21 +4,25 @@ require_relative 'Coin.rb'
 require 'colorize'
 require 'pry'
 
+# Coin flip game
 class DoubleYourMoney
   def initialize(starting_balance)
     @wallet = Wallet.new(starting_balance)
   end
 
+  # Starts the game. Run this after initialize
   def start
     puts 'Welcome to Double Your Money!'.colorize(:green)
     puts 'Type QUIT to stop playing'.colorize(:blue)
     @wallet.print_balance
+    # Play the game until the user quits
     while true
       play_game
     end
   end
 
   def  play_game
+    # Check that the user has put in valid information
     incorrect_flag = false
     user_bet = 0
     until user_bet > 0
@@ -26,39 +30,48 @@ class DoubleYourMoney
         puts "Must be a number larger than 0".colorize(:yellow)
         @wallet.print_balance
       end
+      # Get how much the user wants to bet
       puts 'How much would you like to bet?'
       user_bet = gets.chomp.downcase
+      # Check if the user wants to quit at this point
       quit if user_bet == 'quit'
       user_bet = user_bet.to_f
+      # Make sure that the user has enough money to bet the amount they entered
       unless @wallet.has_amount(user_bet)
         user_bet = 0
         puts "You do not have enough money in your wallet.".colorize(:red)
         @wallet.print_balance
       end
+      # If the loop goes around again it know that the user
+      # entered something wrong. incorrect_flag never need to be reset
+      # as it is thrown away as soon as a correct number is entered.
       incorrect_flag = true
     end
 
     user_selection = ''
+    # Make sure that the user can only enter heads, tails, or quit
     until user_selection == 'heads' || user_selection == 'tails' || user_selection == 'quit'
       puts 'Heads or Tails?'
       user_selection = gets.chomp.downcase
     end
+    # Quit if the user wants
     quit if user_selection == 'quit'
+    # Flip the coin
     result = Coin.flip(user_selection)
     if result
+      # The user picked the winning side
       puts "You Win!".colorize(:green)
       @wallet.add_money(user_bet)
     else
+      # The user picked the loosing side
       puts "Better luck next time!".colorize(:red)
       @wallet.take_money(user_bet)
     end
   end
 
+  # Quit the program. Made a function for less typing elsewhere
   def quit
     puts "Thanks for playing!".colorize(:green)
     exit
   end
 end
-
-test = DoubleYourMoney.new(10.00)
-test.start
