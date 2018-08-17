538034764ba1ce8c00a85b689bae93b2299986e0
Ladd J McClurg <laddjmcclurg@icloud.com>
2018-08-17 15:44:54 -0600

    started menu updated game names

--- main.rb
+++ main.rb
@@ -2,20 +2,19 @@
 ### Start of game program ###
 ###quit menu###
 
-require_relative 'DoubleYourMoney.rb'
+require_relative 'CoinFlip.rb'
 require_relative  #name of next game
 require_relative  #name of next game
 
 def menu
-  puts "Welcome to Ruby Casino"
   puts "Choose A Game"
-  puts "1: Double Your Money"
-  puts "2: Create Contact"
+  puts "1: Coin Flip"
+  puts "2: Slot Machine"
   puts "3 Exit"
   input = gets.strip.to_i
   case 
     when input == 1
-      view_all_contacts
+      
     when input == 2
       create_contact
     when input == 3
