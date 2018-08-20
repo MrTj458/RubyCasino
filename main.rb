501b514e34a83ce864c891bba5f48342383010c7
Trevor H <mrtj458@gmail.com>
2018-08-19 18:11:42 -0600

    Add main menu to a class

--- main.rb
+++ main.rb
@@ -1,6 +1,4 @@
-###menu of games to select###
-### Start of game program ###
-###quit menu###
+# Written by Trevor H. and Ladd M.
 
 require_relative 'CoinFlip.rb'
 require_relative 'SlotMachine.rb'
@@ -8,18 +6,27 @@ require 'colorize'
 require 'artii'
 require 'lolcat'
 
-@wallet = Wallet.new(50.00)
+class Main
+  def initialize
+    @wallet = Wallet.new(50.00)
+  end
 
-def menu
-  system('clear')
-  system('artii \'Ruby Casino\' | lolcat -a -s 500')
-  puts "Pick A Game".colorize(:cyan)
-  puts "1: Coin Flip".colorize(:blue)
-  puts "2: Slot Machine".colorize(:blue)
-  puts "3: Exit".colorize(:red)
-  print '> '
-  input = gets.chomp.to_i
-  case input
+  def start
+    while true
+      menu  
+    end
+  end
+
+  def menu
+    system('clear')
+    system('artii \'Ruby Casino\' | lolcat -a -s 500')
+    puts "Pick A Game".colorize(:cyan)
+    puts "1: Coin Flip".colorize(:blue)
+    puts "2: Slot Machine".colorize(:blue)
+    puts "3: Exit".colorize(:red)
+    print '> '
+    input = gets.chomp.to_i
+    case input
     when 1
       CoinFlip.new(@wallet).start
     when 2
@@ -28,14 +35,14 @@ def menu
       quit
     else 
       puts 'Select Another Game. Select 1 or 2'
+    end
+  end
+
+  def quit
+    system('artii \'Thanks for playing!\' --font slant | lolcat -a -s 500')
+    exit
   end
 end
 
-def quit
-  system('artii \'Thanks for playing!\' --font slant | lolcat -a -s 500')
-  exit
-end
-
-while true
-  menu  
-end
+### Start of program ###
+Main.new.start
